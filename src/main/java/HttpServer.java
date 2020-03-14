8e4dc545011598857e7287b16a4128b0c9bae97a
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 23:50:01 +0300

    Scripted auto-commit on change (2020-03-14 23:50:01) by gitwatch.sh

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -12,12 +12,18 @@ import static akka.http.javadsl.server.Directives.parameter;
 
 public class HttpServer {
 
+    ZooKeeper zoo = new ZooKeeper("1", 3000, this);
+zoo.create("/servers/s",
+        “data”.getBytes(),
+    ZooDefs.Ids.OPEN_ACL_UNSAFE ,
+    CreateMode.EPHEMERAL_SEQUENTIAL
+);
+    List<String> servers = zoo.getChildren("/servers", this);
+for (String s : servers) {
+        byte[] data = zoo.getData("/servers/" + s, false, null);
+        System.out.println("server " + s + " data=" + new String(data));
+    }
 
-
-   zoo = new ZooKeeper(String connectString,
-               int sessionTimeout,
-               Watcher watcher
-    );
 
 
 
