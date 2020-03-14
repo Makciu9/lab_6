9009288504ac1d4da2bde29b3eeb27d684c3d98a
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 19:37:59 +0300

    Scripted auto-commit on change (2020-03-14 19:37:58) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -27,7 +27,7 @@ public class StoreActor extends AbstractActor {
 }
 
     private String getRanS() {
-        return listServers.get(Random((listServers.size())))
+        return listServers.get(random((listServers.size())))
     }
 
 }
