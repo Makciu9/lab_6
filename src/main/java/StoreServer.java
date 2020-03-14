e5094f739e23cf188fc92939407ba9c7b0d7a4ed
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 19:48:41 +0300

    Scripted auto-commit on change (2020-03-14 19:48:41) by gitwatch.sh

--- src/main/java/StoreServer.java
+++ src/main/java/StoreServer.java
@@ -1,6 +1,12 @@
 import java.util.List;
 
 public class StoreServer {
+    private List<String> storeServer;
+
+
+
     public List<String> getServerList() {
+        return storeServer;
     }
+
 }
