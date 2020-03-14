c984c005528195dd0c0f9c61d96d96677f1a0e73
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 19:35:38 +0300

    Scripted auto-commit on change (2020-03-14 19:35:38) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -2,21 +2,36 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class StoreActor extends AbstractActor {
         private Map<String, String> store = new HashMap<>();
+    private List<String> listServers;
 
-        @Override
+    @Override
         public Receive createReceive() {
             return ReceiveBuilder.create()
-                    .match(StoreServer.class, m -> {
+                    .match(StoreServer.class, m -> this.listServers=m.getServerList())
+                    .match(){
                         store.put(m.getKey(), m.getValue());
                         System.out.println("receive message! " + m.toString());
                     })
-                    .match(GetRandomServer.class, req -> sender().tell(
-                            new StoreMessage(req.getKey(), store.get(req.getKey())), self())
-                    ).build();
-        }
+                    .match(GetRandomServer.class, req -> {
+                          String ranS = getRanS();
+                          sender().tell(ranS, store.get(req.getKey())), self());
+
+    }).build();
+
+}
+
+    private String getRanS() {
+        return listServers.get(new Random(nextInt(listServers.size())))
     }
+
+}
+
+
+
 
--- src/main/java/StoreServer.java
+++ src/main/java/StoreServer.java
@@ -1,2 +1,6 @@
+import java.util.List;
+
 public class StoreServer {
+    public List<String> getServerList() {
+    }
 }
