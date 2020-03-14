d8606b60b34eea2c04ffbd3ecc53dbf24319cad4
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 20:16:55 +0300

    Scripted auto-commit on change (2020-03-14 20:16:55) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-public class StoreActor extends AbstractActor {
+public class StorageActor extends AbstractActor {
     private List<String> listServers;
 
     @Override
