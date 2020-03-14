bee4f92abc6a30692585836777e095b0e7f2574f
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 20:35:59 +0300

    Scripted auto-commit on change (2020-03-14 20:35:59) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -35,4 +35,27 @@ for (String s : servers) {
                int sessionTimeout,
                Watcher watcher
     )
+        route(
+                path("semaphore", () ->
+                        route(
+                                get( () -> {
+                                    Future<Object> result = Patterns.ask(testPackageActor,
+                                            SemaphoreActor.makeRequest(), 5000);
+                                    return completeOKWithFuture(result, Jackson.marshaller());
+                                }))),
+                path("test", () ->
+                        route(
+                                post(() ->
+                                        entity(Jackson.unmarshaller(TestPackageMsg.class), msg -> {
+                                            testPackageActor.tell(msg, ActorRef.noSender());
+                                            return complete("Test started!");
+                                        })))),
+                path("put", () ->
+                        get(() ->
+                                parameter("key", (key) ->
+                                        parameter("value", (value) ->
+                                        {
+                                            storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
+                                            return complete("value saved to store ! key=" + key + " value=" + value);
+                                        })))),
 }
