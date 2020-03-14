e8084215c503e9598968c2f7808a7e2220401728
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 23:20:06 +0300

    Scripted auto-commit on change (2020-03-14 23:20:06) by gitwatch.sh

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,4 +1,8 @@
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.*;
 import akka.http.javadsl.server.AllDirectives;
@@ -27,35 +31,7 @@ public class HttpServer {
             materializer
     );
 System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-System.in.read();
-binding
-        .thenCompose(ServerBinding::unbind)
-            .
 
-    thenAccept(unbound ->system.terminate());
-
-    ZooKeeper zoo = new ZooKeeper("1MB27.0.0.1MB:21MB81MB", 3000, this);
-zoo.create("/servers/s",
-        “data”.
-
-    getBytes(),
-
-    ZooDefs.Ids.OPEN_ACL_UNSAFE ,
-    CreateMode.EPHEMERAL_SEQUENTIAL
-);
-    List<String> servers = zoo.getChildren("/servers", this);
-for(
-    String s :servers)
-
-    {
-        byte[] data = zoo.getData("/servers/" + s, false, null);
-        System.out.println("server " + s + " data=" + new String(data));
-
-        ZooKeeper(String connectString,
-        int sessionTimeout,
-        Watcher watcher
-    )
-    }
 
     public Route createRoute(){
         return route(
