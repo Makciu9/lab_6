48695f4303b1d169e52b61741de5919817ad5112
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 20:22:22 +0300

    Scripted auto-commit on change (2020-03-14 20:22:22) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,12 @@
+import org.apache.zookeeper.Watcher;
+
 public class Server {
+
+
+    
+
+    ZooKeeper(String connectString,
+               int sessionTimeout,
+               Watcher watcher
+    )
 }
