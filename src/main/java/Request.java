1bed9eca31f16d62209549cf3966b19ec97b7f7e
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-15 14:43:41 +0300

    Scripted auto-commit on change (2020-03-15 14:43:41) by gitwatch.sh

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,3 +1,23 @@
 public class Request {
-    
+    public String url;
+    public int count;
+
+    Request(String url, String count){
+        this.url = url;
+        this.count= Integer.parseInt(count);
+    }
+
+    Request(String url, int count){
+        this.url = url;
+        this.count = count;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
 }
