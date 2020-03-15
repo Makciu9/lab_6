3d5e00029f1eb42f1951b8f67a6e5b90a3c6c5a8
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-15 03:50:02 +0300

    Scripted auto-commit on change (2020-03-15 03:50:02) by gitwatch.sh

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -38,7 +38,7 @@ public class HttpServer {
 
 
     final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app ;
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute ;
 
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
