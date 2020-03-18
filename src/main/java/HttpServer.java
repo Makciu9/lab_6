8975066e433cb9386bcd46978f17d2966b1600a2
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-18 16:14:39 +0300

    Scripted auto-commit on change (2020-03-18 16:14:39) by gitwatch.sh

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -111,7 +111,7 @@ public class HttpServer extends AllDirectives {
                     Integer.toString(port).getBytes(),
                     ZooDefs.Ids.OPEN_ACL_UNSAFE,
                     CreateMode.EPHEMERAL_SEQUENTIAL);
-            
+
 
         }
 
