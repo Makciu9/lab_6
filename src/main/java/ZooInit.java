a596e7527b5c998ea3d67cbad7b59bde326c7ad5
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-18 20:47:30 +0300

    Scripted auto-commit on change (2020-03-18 20:47:30) by gitwatch.sh

--- src/main/java/ZooInit.java
+++ src/main/java/ZooInit.java
@@ -27,7 +27,7 @@ public  class ZooInit implements Watcher {
 
 
     public void createZoo(String LOCALHOST, String port) throws KeeperException, InterruptedException {
-        String path = zoo.create("/servers5/" + LOCALHOST + ":" + port,
+        String path = zoo.create("/servers5/s" + LOCALHOST + ":" + port,
                 port.getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL_SEQUENTIAL);
