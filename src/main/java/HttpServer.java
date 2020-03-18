006cd018737f3496c622232f2703da49445e71b3
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-18 18:49:18 +0300

    Scripted auto-commit on change (2020-03-18 18:49:18) by gitwatch.sh

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -38,11 +38,10 @@ public class HttpServer extends AllDirectives {
         final ActorMaterializer materialize = ActorMaterializer.create(system);
 
         ZooWatcher zooWat = new ZooWatcher();
-        final Http http = Http.get(system);
-
-
         ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000, zooWat);
 
+        final Http http = Http.get(system);
+        
         ZooInit app = new ZooInit(zoo, storeActor, http);
         app.createZoo(LOCALHOST, port);
 
